d17e3378e3380ff4481fb79196cfa9eb500bf980
wentdavid <davidwent@me.com>
2022-09-20 15:35:01 +0200

    typo fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 let pokemonList=[{name: 'Machamp',height: '1.6',types: ['Fighting']}, 
                 {name: 'Magneton',height: '1',types: ['Electric','Steel']},
                 {name: 'Shiftry',height: '1.3',types: ['Dark','Grass']},
-                {name: 'Charizard',height: '1.7',types: ['Fire','Flying']},]
+                {name: 'Charizard',height: '1.7',types: ['Fire','Flying']}]
