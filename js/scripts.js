35de0eb4b3b81afc4dec27f900e0eb5017564c72
wentdavid <davidwent@me.com>
2022-09-26 14:27:38 +0200

    added forEach method for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,18 @@
 // Pokemon list with items
+
+
 let pokemonList=[{name: 'Machamp',height: '1.6',types: ['Fighting']}, 
                 {name: 'Magneton',height: '1',types: ['Electric','Steel']},
                 {name: 'Shiftry',height: '1.3',types: ['Dark','Grass']},
                 {name: 'Charizard',height: '1.7',types: ['Fire','Flying']}]
 
+pokemonList.forEach (function(pokemon) {
+    console.log (pokemon.name + 's height is ' + pokemon.height + '<br>' );
+    document.write (pokemon.name + 's height is ' + pokemon.height + '<br>' );
+});
 
 
 
 
 
-function divide (dividend, divisor) {
-    if(divisor === 0) {
-    return 'You are trying to divide by zero.'
-    
 
-    }else{ 
-        let result = dividend / divisor;
-        return result;
-}
-}
