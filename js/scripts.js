b8ce0cdb9e0b822eb5de02c035e47e3aa8a77f30
wentdavid <davidwent@me.com>
2022-09-26 16:34:46 +0200

    added return for ad function and getAll function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 // Pokemon list with items
 
-
-let pokemonList=[{name: 'Machamp',height: '1.6',types: ['Fighting']}, 
-                {name: 'Magneton',height: '1',types: ['Electric','Steel']},
-                {name: 'Shiftry',height: '1.3',types: ['Dark','Grass']},
-                {name: 'Charizard',height: '1.7',types: ['Fire','Flying']}]
+//IIFE
+let pokemonRepository = (function () {
+    let pokemonList = []; // empty array
+  
+    return {
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      },
+      getAll: function() {
+        return pokemonList;
+      }
+    };
+  })();
 
 pokemonList.forEach (function(pokemon) {
     console.log (pokemon.name + 's height is ' + pokemon.height + '<br>' );
@@ -16,3 +24,7 @@ pokemonList.forEach (function(pokemon) {
 
 
 
+/* let pokemonList=[{name: 'Machamp',height: '1.6',types: ['Fighting']}, 
+                    {name: 'Magneton',height: '1',types: ['Electric','Steel']},
+                    {name: 'Shiftry',height: '1.3',types: ['Dark','Grass']},
+                    {name: 'Charizard',height: '1.7',types: ['Fire','Flying']}] */
