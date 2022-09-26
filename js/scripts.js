1dad9da3b06cce2f4ae717505343fa44bbbc6a3c
wentdavid <davidwent@me.com>
2022-09-26 18:45:30 +0200

    for each function for pokemonRepository added

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,37 @@
 // Pokemon list with items
 
-//IIFE
+
+
+//IIFE pokemonRepository and pokemonList
 let pokemonRepository = (function () {
-    let pokemonList = []; // empty array
+    let pokemonList= 
+                    [{name: 'Machamp',height: '1.6',types: ['Fighting']}, 
+                    {name: 'Magneton',height: '1',types: ['Electric','Steel']},
+                    {name: 'Shiftry',height: '1.3',types: ['Dark','Grass']},
+                    {name: 'Charizard',height: '1.7',types: ['Fire','Flying']}];
+
+    function getAll() {
+    return pokemonList;
+    }
+    
+    function add(pokemon) {
+    pokemonList.push(pokemon);
+    }
   
     return {
-      add: function(pokemon) {
-        pokemonList.push(pokemon);
-      },
-      getAll: function() {
-        return pokemonList;
-      }
+        getAll: getAll
+        add: add,
+      
     };
-  })();
 
-pokemonList.forEach (function(pokemon) {
+  })();
+  
+
+// forEach function for pokemonlist
+pokemonRepository.getAll().forEach (function(pokemon) {
     console.log (pokemon.name + 's height is ' + pokemon.height + '<br>' );
     document.write (pokemon.name + 's height is ' + pokemon.height + '<br>' );
 });
 
-
-
-
-
-
-/* let pokemonList=[{name: 'Machamp',height: '1.6',types: ['Fighting']}, 
-                    {name: 'Magneton',height: '1',types: ['Electric','Steel']},
-                    {name: 'Shiftry',height: '1.3',types: ['Dark','Grass']},
-                    {name: 'Charizard',height: '1.7',types: ['Fire','Flying']}] */
+pokemonRepository.add(pokemon);
+    
