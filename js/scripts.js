9dbb31bfc1756626667cb0cbf7cce7c469fc84cb
wentdavid <davidwent@me.com>
2022-10-27 12:10:56 +0200

    added search function and pagination

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,18 @@
 //IIFE pokemonRepository and pokemonList from API
 const pokemonRepository = (function() {
 	const pokemonList = [];
-	const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=18';
+	let prevURL = '';
+	let nextURL = ''; 
 	
 	//Fetching pokemon list
-	function loadList() {
+	function loadList(apiUrl) {
 		return fetch(apiUrl).then(function(response) {
 			return response.json();
 		}).then(function(json) {
+			console.log("nextUIRL", json.next)
+			prevURL = json.previous;
+			nextURL = json.next;
+			pokemonList.length = 0;
 			json.results.forEach(function(item) {
 				let pokemon = {
 					name: item.name,
@@ -28,17 +33,29 @@ const pokemonRepository = (function() {
 			typeof pokemon === 'object' &&
 			'name' in pokemon
 		) {
-
 			pokemonList.push(pokemon);
 		} else {
 			console.log('Pokemon is not correct');
 		}
 	};
 
+	function clearList() {
+		let pokemonList = document.querySelector('.pokemon-list');
+		pokemonList.innerHTML = '';
+	}
+
 	//Returning the pokemonList array
 	function getAll() {
 		return pokemonList;
 	};
+
+	function getNextURL() {
+		return nextURL;
+	}
+
+	function getPrevURL() {
+		return prevURL;
+	}
 
 	//Adding pokemon items into buttons into .pokemon-list on the index page
 	function addListItem(pokemon) {
@@ -63,13 +80,20 @@ const pokemonRepository = (function() {
 	const searchInput = document.querySelector('[data-search]')
 
 	searchInput.addEventListener('input', e => {
-	const value = e.target.value.toLowerCase()
-	pokemonList.forEach(item => {
-		const isVisible =
-		item.name.toLowerCase().includes(value) 
-		button.classList.toggle('hide', !isVisible)
+		const value = e.target.value.toLowerCase()
+		const pokemonList = document.querySelectorAll('.pokemon-list-button');
+		pokemonList.forEach(pokemon => {
+			if(pokemon.innerText.toLowerCase().includes(value)) {
+				pokemon.style.display = '';
+			}
+			else {
+				pokemon.style.display = 'none';
+			}
+		})
 	})
-	})
+
+
+
 
 	//Fetching details of a single pokemon from API.
 	function loadDetails(item) {
@@ -105,10 +129,51 @@ const pokemonRepository = (function() {
 		loadList,
 		loadDetails,
 		showModal,
+		getNextURL,
+		getPrevURL,
+		clearList,
 	};
 })();
 
-pokemonRepository.loadList().then(function() {
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=18';
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+console.log(prevBtn)
+
+prevBtn.addEventListener('click', (e) => {
+	const prevURL = pokemonRepository.getPrevURL();
+	if (!prevURL) {
+		return;
+	}
+	pokemonRepository.loadList(prevURL).then(function() {
+		pokemonRepository.clearList();
+		// Now the data is loaded!
+		// forEach function for pokemonlist
+		pokemonRepository.getAll().forEach(function(pokemon) {
+			pokemonRepository.addListItem(pokemon);
+		});
+	});
+	console.log(pokemonRepository.getPrevURL());
+});
+
+nextBtn.addEventListener('click', (e) => {
+	const nextURL = pokemonRepository.getNextURL();
+	if (!nextURL) {
+		return;
+	}
+	pokemonRepository.loadList(nextURL).then(function() {
+		pokemonRepository.clearList();
+		// Now the data is loaded!
+		// forEach function for pokemonlist
+		pokemonRepository.getAll().forEach(function(pokemon) {
+			pokemonRepository.addListItem(pokemon);
+		});
+	});
+	console.log(pokemonRepository.getNextURL());
+});
+
+pokemonRepository.loadList(apiUrl).then(function() {
 	// Now the data is loaded!
 	// forEach function for pokemonlist
 	pokemonRepository.getAll().forEach(function(pokemon) {
